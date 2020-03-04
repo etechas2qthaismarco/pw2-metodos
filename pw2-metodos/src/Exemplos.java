ebce1c54f3fa0c497b9729243ecd29b22d3eb232
etechas2qthaismarco <thais.viana5@etec.sp.gov.br>
2020-03-04 20:37:24 -0300

    Exemplos de metodos

--- pw2-metodos/src/Exemplos.java
+++ pw2-metodos/src/Exemplos.java
@@ -1,14 +1,33 @@
 
 public class Exemplos {
-	
+
 	public static void main(String[] args) {
-		int idade = 17;
-		
+		ehMaiorIdade(17);
+		classificar(-1);
+
+	}
+
+	public static void ehMaiorIdade(int idade) {
+
 		if (idade >= 18) {
-			System.out.println("Maior de idade");
+			System.out.println("Maior");
 		} else {
-			System.out.println("Menor de idade");
+			System.out.println("Menor");
+
 		}
 	}
 
+	public static void classificar(int num) {
+		if (num > 0) {
+			System.out.println("Positivo");
+
+		} else if (num < 0) {
+			System.out.println("Negativo");
+
+		} else {
+			System.out.println("Zero");
+
+		}
+
+	}
 }
